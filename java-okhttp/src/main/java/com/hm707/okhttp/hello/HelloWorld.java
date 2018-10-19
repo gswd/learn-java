b809180651de61b0c1199199ace4607478dfb5d5
lihao <li-hao@navercorp.com>
2018-10-19 18:13:09 +0800

    hello okHttp.

--- java-okhttp/src/main/java/com/hm707/okhttp/hello/HelloWorld.java
+++ java-okhttp/src/main/java/com/hm707/okhttp/hello/HelloWorld.java
@@ -2,6 +2,9 @@ package com.hm707.okhttp.hello;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -9,13 +12,19 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class HelloWorld {
+
+	private static final OkHttpClient client = new OkHttpClient();
+
 	public static void main(String[] args) throws IOException {
 		String getUrl = "https:www.baidu.com";
-		getMethod(getUrl);
+		//getMethod(getUrl);
+
+		//syncGet();
+		//asyncGet();
+		accessHeaders();
 	}
 
 	private static void getMethod(String url) throws IOException {
-		OkHttpClient client = new OkHttpClient();
 
 		Request request = new Request.Builder().url(url).build();
 
@@ -30,7 +39,6 @@ public class HelloWorld {
 	}
 
 	private static void postMethod(String url, String json) throws IOException {
-		OkHttpClient client = new OkHttpClient();
 		RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json);
 
 		Request request = new Request.Builder().url(url).post(body).build();
@@ -44,6 +52,70 @@ public class HelloWorld {
 			throw new IOException("Unexpected code " + response);
 		}
 
+	}
+
+	private static void syncGet() throws IOException {
+		Request request = new Request.Builder().url("http://publicobject.com/helloworld.txt").build();
+
+		Response response = client.newCall(request).execute();
+		if (!response.isSuccessful()) {
+			throw new IOException("Unexpected code " + response);
+		}
+
+		Headers responseHeaders = response.headers();
+
+		for (String n : responseHeaders.names()) {
+			System.out.println(n + ":" + responseHeaders.get(n));
+		}
+
+		System.out.println(response.body().string());
+
+	}
+
+	private static void asyncGet() throws IOException {
+		Request request = new Request.Builder().url("http://publicobject.com/helloworld.txt").build();
+
+		client.newCall(request).enqueue(new Callback() {
+
+			@Override
+			public void onFailure(Call call, IOException e) {
+				e.printStackTrace();
+			}
+
+			@Override
+			public void onResponse(Call call, Response response) throws IOException {
+				if (!response.isSuccessful()) {
+					throw new IOException("Unexpected code " + response);
+				}
+
+				Headers responseHeaders = response.headers();
+
+				for (String n : responseHeaders.names()) {
+					System.out.println(n + ":" + responseHeaders.get(n));
+				}
+
+				System.out.println(response.body().string());
+			}
+		});
+
+	}
+
+	private static void accessHeaders() throws IOException{
+		Request request = new Request.Builder()
+				.url("https://api.github.com/repos/square/okhttp/issues")
+				.header("User-Agent", "OkHttp Headers.java")
+				.addHeader("Accept", "application/json; q=0.5")
+				.addHeader("Accept", "application/vnd.github.v3+json")
+				.build();
+
+		Response response = client.newCall(request).execute();
+		if (!response.isSuccessful()) {
+			throw new IOException("Unexpected code " + response);
+		}
+
+		System.out.println("Server: " + response.header("Server"));
+		System.out.println("Date: " + response.header("Date"));
+		System.out.println("Vary: " + response.headers("Vary"));
 	}
 
 }
