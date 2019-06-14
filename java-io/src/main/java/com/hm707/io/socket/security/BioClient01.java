adafc193140005a9ce94f534f71a9fc355e78595
lihao <lihao411@163.com>
2019-06-14 18:13:36 +0800

    bio ssl socket.

--- java-io/src/main/java/com/hm707/io/socket/security/BioClient01.java
+++ java-io/src/main/java/com/hm707/io/socket/security/BioClient01.java
@@ -1,11 +1,9 @@
 package com.hm707.io.socket.security;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.nio.file.Paths;
 
 import javax.net.SocketFactory;
 import javax.net.ssl.SSLSocketFactory;
--- java-io/src/main/java/com/hm707/io/socket/security/BioServer01.java
+++ java-io/src/main/java/com/hm707/io/socket/security/BioServer01.java
@@ -6,8 +6,6 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import javax.net.ssl.SSLContext;
 
