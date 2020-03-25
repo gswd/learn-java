679fe81e508643571003a3f6ea1c95e29ad1a516
lihao <lihao411@163.com>
2020-03-25 14:34:05 +0800

    eee..

--- intellij-pligin/src/Test01.java
+++ intellij-pligin/src/Test01.java
@@ -2,26 +2,28 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
+import com.intellij.psi.PsiClass;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.psi.JavaRecursiveElementVisitor;
-import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiLocalVariable;
-import com.intellij.psi.PsiMethod;
 import com.intellij.psi.util.PsiTreeUtil;
 
 public class Test01 extends AnAction {
 	protected static final Logger log = Logger.getInstance(Test01.class);
+
 	@Override
 	public void actionPerformed(AnActionEvent e) {
-		log.info("------- > actionPerformed ....");
+//		PsiDocumentManager.getInstance().getDocument();
+
+	}
+//	@Override
+//	public void actionPerformed(AnActionEvent e) {
+//		log.info("------- > actionPerformed ....");
 //		Application application = ApplicationManager.getApplication();
 //		MyComponent myComponent = application.getComponent(MyComponent.class);
 //		myComponent.sayHello();
@@ -41,55 +43,12 @@ public class Test01 extends AnAction {
 //		System.out.println("---------testEditor----------");
 //		testEditor(e);
 
-		System.out.println("---------testPSI----------");
-		testPSI(e);
 
-		Project project = e.getData(PlatformDataKeys.PROJECT);
-		String txt= Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
-		Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
-	}
+//		Project project = e.getData(PlatformDataKeys.PROJECT);
+//		String txt= Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
+//		Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+//	}
 
-	private void testPSI(AnActionEvent anActionEvent) {
-		Editor editor = anActionEvent.getData(PlatformDataKeys.EDITOR);
-		PsiFile psiFile = anActionEvent.getData(CommonDataKeys.PSI_FILE);
-		if (editor == null ||psiFile == null) {
-
-			System.out.println("-----psiFile ---> " + psiFile);
-			System.out.println("-----editor ---> " + editor);
-			return;
-		}
-		int offset = editor.getCaretModel().getOffset();
-		final StringBuilder infoBuilder = new StringBuilder();
-		PsiElement element = psiFile.findElementAt(offset);
-		infoBuilder.append("Element at caret: ").append(element).append("\n");
-		if (element != null) {
-			PsiMethod containingMethod = PsiTreeUtil.getParentOfType(element, PsiMethod.class);
-			infoBuilder
-				.append("Containing method: ")
-				.append(containingMethod != null ? containingMethod.getName() : "none")
-				.append("\n");
-			if (containingMethod != null) {
-				PsiClass containingClass = containingMethod.getContainingClass();
-				infoBuilder
-					.append("Containing class: ")
-					.append(containingClass != null ? containingClass.getName() : "none")
-					.append("\n");
-
-				infoBuilder.append("Local variables:\n");
-				containingMethod.accept(new JavaRecursiveElementVisitor() {
-					@Override
-					public void visitLocalVariable(PsiLocalVariable variable) {
-						super.visitLocalVariable(variable);
-						infoBuilder.append(variable.getName()).append("\n");
-					}
-				});
-			}
-		}
-		System.out.println(infoBuilder.toString());
-		Messages.showMessageDialog(anActionEvent.getProject(), infoBuilder.toString(), "PSI Info", null);
-
-
-	}
 
 	private void testEditor(AnActionEvent anActionEvent) {
 		Editor editor = anActionEvent.getData(PlatformDataKeys.EDITOR);
--- java-se/src/main/java/com/hm707/basic/BasicOperation.java
+++
@@ -1,9 +0,0 @@
-package com.hm707.basic;
-
-public class BasicOperation {
-	public static void main(String[] args) {
-
-		int r1 = 0x0001 << 3;
-		System.out.println(Integer.toBinaryString(r1));
-	}
-}
