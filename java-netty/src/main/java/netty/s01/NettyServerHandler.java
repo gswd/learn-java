a34fda2b13a6f9c2fdc8f94e77a9a00c22c02c1e
lihao <lihao411@163.com>
2020-03-30 18:33:48 +0800

    netty

--- java-netty/src/main/java/netty/s01/NettyServerHandler.java
+++ java-netty/src/main/java/netty/s01/NettyServerHandler.java
@@ -1,5 +1,7 @@
 package netty.s01;
 
+import java.util.concurrent.TimeUnit;
+
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
@@ -11,15 +13,38 @@ import io.netty.util.CharsetUtil;
 public class NettyServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("服务器读取线程 : " + Thread.currentThread().getName());
-        System.out.println("server ctx = " + ctx);
+//        System.out.println("服务器读取线程 : " + Thread.currentThread().getName());
+//        System.out.println("server ctx = " + ctx);
 
         Channel channel = ctx.channel();
         ChannelPipeline pipeline = ctx.pipeline();//双向链表
 
         ByteBuf buf = (ByteBuf) msg;
         System.out.println("client send message : " + buf.toString(CharsetUtil.UTF_8));
-        System.out.println("client addr : " + ctx.channel().remoteAddress());
+
+        ctx.channel().eventLoop().execute(()->{
+            System.out.println(System.currentTimeMillis() + " : " + Thread.currentThread().getName() + " - 业务处理开始1");
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(System.currentTimeMillis() + " : " + Thread.currentThread().getName() + " - 业务处理完成1.");
+
+        });
+//        ctx.channel().eventLoop().schedule(
+//            ()->{
+//                System.out.println("业务处理开始2" + Thread.currentThread().getName());
+//                try {
+//                    TimeUnit.SECONDS.sleep(2);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//            }
+//            System.out.println("业务处理完成2.");
+//
+//        }, 10, TimeUnit.SECONDS);
+//        System.out.println("111");
+
     }
 
     @Override
